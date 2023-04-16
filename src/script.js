ec569f29f2e2e14672872eee4bafc19ec4dcb39c
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-16 11:59:25 +0900

    feat: calclate complement

--- src/script.js
+++ src/script.js
@@ -13,10 +13,19 @@ function clickNumButton(x) {
     }
     console.log(binList);
 
+    binListHosu = binList.concat();
+    for (let i = 0; i < binList.length; i++) {
+        binListHosu[i] = 1 - binListHosu[i];
+    }
+
     dec = 0;
+    decM = 0;
     for (let i = binList.length - 1; i > 0; i--) {
         dec += Math.pow(2, binList.length - 1 - i) * binList[i];
+        decM += Math.pow(2, binList.length - 1 - i) * binListHosu[i];
     }
     dec -= Math.pow(2, binList.length - 1) * binList[0];
+    decM -= Math.pow(2, binList.length - 1) * binListHosu[0];
     console.log(dec);
+    console.log(decM);
 }
