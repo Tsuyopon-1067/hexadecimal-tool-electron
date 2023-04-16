db42fb516598e0066fd11cc4811b6a05797576b4
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-16 12:30:44 +0900

    refactor: functionalize the group of the process

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 binList = [];
 
 function clickNumButton(x) {
+    // 押されたボタンに応じて2進数配列に要素を加える
     for (let i = 3; i >= 0; i--) {
         binTmp = Math.pow(2, i);
         x -= binTmp;
@@ -13,42 +14,20 @@ function clickNumButton(x) {
     }
     console.log(binList);
 
-    binListHosu = binList.concat();
-    for (let i = 0; i < binList.length; i++) {
-        binListHosu[i] = 1 - binListHosu[i];
-    }
+    // 2の補数を求める
+    binListHosu = getBinComplement(binList);
 
     // 2進数のテキストを用意する
-    binText = "0b";
-    binTextHosu = "0b";
-    for (let i = 0; i < binList.length; i++) {
-        binText += binList[i].toString();
-        binTextHosu += binListHosu[i].toString();
-    }
+    binText = getBinText(binList);
+    binTextHosu = getBinText(binListHosu);
 
     // 10進数に変換する
-    dec = 0;
-    decHosu = 0;
-    for (let i = binList.length - 1; i > 0; i--) {
-        dec += Math.pow(2, binList.length - 1 - i) * binList[i];
-        decHosu += Math.pow(2, binList.length - 1 - i) * binListHosu[i];
-    }
-    dec -= Math.pow(2, binList.length - 1) * binList[0];
-    decHosu -= Math.pow(2, binList.length - 1) * binListHosu[0];
+    dec = getDec(binList);
+    decHosu = getDec(binListHosu);
 
     // 16進数に変換する
-    hexText = "0x";
-    hexTextHosu = "0x";
-    for (let i = 0; i < binList.length; i += 4) {
-        tmp = 0;
-        tmpHosu = 0;
-        for (let j = 0; j < 4; j++) {
-            tmp += Math.pow(2, 3 - j) * binList[i + j];
-            tmpHosu += Math.pow(2, 3 - j) * binListHosu[i + j];
-        }
-        hexText += getHexToken(tmp);
-        hexTextHosu += getHexToken(tmpHosu);
-    }
+    hexText = getHex(binList);
+    hexTextHosu = getHex(binListHosu);
 
     console.log(dec);
     console.log(decHosu);
@@ -56,6 +35,47 @@ function clickNumButton(x) {
     console.log(binTextHosu);
     console.log(hexText);
     console.log(hexTextHosu);
+}
+
+function getBinComplement(lst) {
+    // 2の補数を求める
+    res = lst.concat();
+    for (let i = 0; i < lst.length; i++) {
+        res[i] = 1 - lst[i];
+    }
+    return res;
+}
+
+function getBinText(lst) {
+    // ２進数配列を文字列化する
+    res = "0b";
+    for (let i = 0; i < lst.length; i++) {
+        res += lst[i].toString();
+    }
+    return res;
+}
+
+function getDec(lst) {
+    // 10進数に変換する
+    res = 0;
+    for (let i = lst.length - 1; i > 0; i--) {
+        res += Math.pow(2, lst.length - 1 - i) * lst[i];
+    }
+    res -= Math.pow(2, lst.length - 1) * lst[0];
+    return res;
+}
+
+function getHex(lst) {
+    // 16進数に変換する
+    res = "0x";
+    for (let i = 0; i < lst.length; i += 4) {
+        tmp = 0;
+        for (let j = 0; j < 4; j++) {
+            tmp += Math.pow(2, 3 - j) * lst[i + j];
+        }
+        res += getHexToken(tmp);
+    }
+    return res;
 }
 
 function getHexToken(x) {
