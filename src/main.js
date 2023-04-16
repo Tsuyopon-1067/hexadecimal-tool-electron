313fd70b5ea65cc867b244764546f2ebc7c9c439
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-16 15:52:02 +0900

    update: layout

--- src/main.js
+++ src/main.js
@@ -6,47 +6,48 @@ const path = require("path");
 let mainWindow;
 
 const createWindow = () => {
-  // メインウィンドウを作成します
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // プリロードスクリプトは、レンダラープロセスが読み込まれる前に実行され、
-      // レンダラーのグローバル（window や document など）と Node.js 環境の両方にアクセスできます。
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+    // メインウィンドウを作成します
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 800,
+        webPreferences: {
+            // プリロードスクリプトは、レンダラープロセスが読み込まれる前に実行され、
+            // レンダラーのグローバル（window や document など）と Node.js 環境の両方にアクセスできます。
+            preload: path.join(__dirname, "preload.js"),
+        },
+        titleBarStyle: 'hidden'
+    });
 
-  // メインウィンドウに表示するURLを指定します
-  // （今回はmain.jsと同じディレクトリのindex.html）
-  mainWindow.loadFile("index.html");
+    // メインウィンドウに表示するURLを指定します
+    // （今回はmain.jsと同じディレクトリのindex.html）
+    mainWindow.loadFile("index.html");
 
-  // デベロッパーツールの起動
-  mainWindow.webContents.openDevTools();
+    // デベロッパーツールの起動
+    mainWindow.webContents.openDevTools();
 
-  // メインウィンドウが閉じられたときの処理
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+    // メインウィンドウが閉じられたときの処理
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
 };
 
 //  初期化が完了した時の処理
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
-  app.on("activate", () => {
-    // メインウィンドウが消えている場合は再度メインウィンドウを作成する
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+    // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+    app.on("activate", () => {
+        // メインウィンドウが消えている場合は再度メインウィンドウを作成する
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 // 全てのウィンドウが閉じたときの処理
 app.on("window-all-closed", () => {
-  // macOSのとき以外はアプリケーションを終了させます
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    // macOSのとき以外はアプリケーションを終了させます
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
