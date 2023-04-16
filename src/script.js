43035f3fe1740892662d2d0a7a30db4d7a671402
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-16 12:11:23 +0900

    feat: convert to hexdecimal

--- src/script.js
+++ src/script.js
@@ -18,14 +18,56 @@ function clickNumButton(x) {
         binListHosu[i] = 1 - binListHosu[i];
     }
 
+    // 10進数に変換する
     dec = 0;
-    decM = 0;
+    decHosu = 0;
     for (let i = binList.length - 1; i > 0; i--) {
         dec += Math.pow(2, binList.length - 1 - i) * binList[i];
-        decM += Math.pow(2, binList.length - 1 - i) * binListHosu[i];
+        decHosu += Math.pow(2, binList.length - 1 - i) * binListHosu[i];
     }
     dec -= Math.pow(2, binList.length - 1) * binList[0];
-    decM -= Math.pow(2, binList.length - 1) * binListHosu[0];
+    decHosu -= Math.pow(2, binList.length - 1) * binListHosu[0];
+
+    // 16進数に変換する
+    hexText = "0x";
+    hexTextHosu = "0x";
+    for (let i = 0; i < binList.length; i += 4) {
+        tmp = 0;
+        tmpHosu = 0;
+        for (let j = 0; j < 4; j++) {
+            tmp += Math.pow(2, 3 - j) * binList[i + j];
+            tmpHosu += Math.pow(2, 3 - j) * binListHosu[i + j];
+        }
+        hexText += getHexToken(tmp);
+        hexTextHosu += getHexToken(tmpHosu);
+    }
+
     console.log(dec);
-    console.log(decM);
+    console.log(decHosu);
+    console.log(hexText);
+    console.log(hexTextHosu);
+}
+
+function getHexToken(x) {
+    if (x < 10) {
+        return x.toString();
+    } else {
+        switch (x) {
+            case 10:
+                return "A";
+            case 11:
+                return "B";
+            case 12:
+                return "C";
+            case 13:
+                return "D";
+            case 14:
+                return "E";
+            case 15:
+                return "F";
+
+            default:
+                return "";
+        }
+    }
 }
