33e0826e5141030701fec81689cc2f56fe135d20
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-16 11:50:50 +0900

    refactor: delete unnecessary variable

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
-keta = 0;
 binList = [];
 
 function clickNumButton(x) {
-    keta += 4;
     for (let i = 3; i >= 0; i--) {
         binTmp = Math.pow(2, i);
         x -= binTmp;
@@ -16,9 +14,9 @@ function clickNumButton(x) {
     console.log(binList);
 
     dec = 0;
-    for (let i = keta - 1; i > 0; i--) {
-        dec += Math.pow(2, keta - 1 - i) * binList[i];
+    for (let i = binList.length - 1; i > 0; i--) {
+        dec += Math.pow(2, binList.length - 1 - i) * binList[i];
     }
-    dec -= Math.pow(2, keta - 1) * binList[0];
+    dec -= Math.pow(2, binList.length - 1) * binList[0];
     console.log(dec);
 }
